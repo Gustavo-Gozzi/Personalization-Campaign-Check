bdeb9c9b56000ba616388ed9d1fb95150356829c
Gustavo Ranéa Gozzi <gozzigustavo@gmail.com>
2025-09-12 18:00:17 -0300

    alteracao rotas

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from src.routes import teste
+from flask import Flask
 
-print(teste())
+def create_app():
+    app = Flask(__name__)
+
+    register_routes(app)
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,13 @@
 from src.Application.Controller.user_control import Controller
+from flask import jsonify, make_response
 
 def teste():
     return Controller.getData()
 
+def validation_routes(app):
+    @app.route('/mcp/campaign', methods=['POST'])
+    def validation():
+        return Controller.getData()
 
 if __name__ == '__main__':
     teste()
